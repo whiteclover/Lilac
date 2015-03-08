4eb0e3296c2e08e29fb7fb178dee58d14417d7ba
Thomas <lyanghwy@gmail.com>
2015-03-08 23:32:33 +0800

    Fixed paginator

--- lilac/paginator.py
+++ lilac/paginator.py
@@ -3,45 +3,47 @@
 
 class Paginator(object):
 
-    def __init__(self, results, total, page, perpage, url, gule='?page='):
+    def __init__(self, results, total, page, perpage, url, glue='?page='):
         self.results = results
         self.total = total
         self.page = page
         self.first = 'First'
         self.last = 'Last'
-        self.next = 'Next'
-        self.prev = 'Previous'
+        self._next = 'Next'
+        self._prev = 'Previous'
         self.perpage = perpage
         self.url = url
         self._index = 0
-        self.gule = gule
+        self.gule = glue
 
     def next_link(self, text=None, default=''):
-        text = text or self.text
+        text = text or self._next
         pages = (self.total / self.perpage) + 1
         if self.page < pages:
             page = self.page + 1
-            return '<a href="' + self.url + self.gule + str(page) + '">' + text + '</a>'
+            return '<a href="' + self.url + self.glue + str(page) + '">' + text + '</a>'
 
         return default
 
     def pre_link(self, text=None, default=''):
-        text = text or self.prev
+        text = text or self._prev
         if self.page > 1:
             page = self.page - 1
-            return '<a href="' + self.url + self.gule + str(page) + '">' + text + '</a>'
+            return '<a href="' + self.url + self.glue + str(page) + '">' + text + '</a>'
 
         return default
 
     def links(self):
         html = ''
-        pages = (self.total / self.perpage) + 1
+        pages = (self.total / self.perpage)
+        if self.total % self.perpage != 0:
+            pages += 1
         ranged = 4
         if pages > 1:
             if self.page > 1:
                 page = self.page - 1
                 html += '<a href="' + self.url + '">' + self.first + '</a>' + \
-                    '<a href="' + self.url + self.gule + str(page) + '">' + self.prev + '</a>'
+                    '<a href="' + self.url + self.glue + str(page) + '">' + self._prev + '</a>'
             for i in range(self.page - ranged, self.page + ranged):
                 if i < 0:
                     continue
@@ -52,12 +54,12 @@ class Paginator(object):
                 if page == self.page:
                     html += '<strong id="current-page">' + str(page) + '</strong>'
                 else:
-                    html += '<a href="' + self.url + self.gule + str(page) + '">' + str(page) + '</a>'
+                    html += '<a href="' + self.url + self.glue + str(page) + '">' + str(page) + '</a>'
 
             if self.page < pages:
                 page = self.page + 1
 
-                html += '<a href="' + self.url + self.gule + str(page) + '">' + self.next + '</a> <a href="' + \
+                html += '<a href="' + self.url + self.glue + str(page) + '">' + self._next + '</a> <a href="' + \
                     self.url + self.gule + str(pages) + '">' + self.last + '</a>'
 
         return html
